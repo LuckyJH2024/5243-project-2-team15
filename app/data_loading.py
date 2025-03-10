c1a2a682ccacf9f90a7e7f8971e0b5d76484f727
nadiawoo <shanghim@gmail.com>
2025-03-10 02:22:01 -0400

    adding parsing for json files

--- app/data_loading.py
+++ app/data_loading.py
@@ -1,5 +1,6 @@
 from shiny import ui, reactive, render
 import pandas as pd
+import json
 
 # UI for data loading
 data_loading_ui = ui.nav_panel(
@@ -15,15 +16,20 @@ data_loading_ui = ui.nav_panel(
 			placeholder = "No file selected",
 		),
 		ui.input_action_button("process", "Process Data"),
-		ui.output_text("file_name")
-	    ),
-		width = 350
-	), 
-	ui.output_table("data_preview")
+		ui.output_text("file_name"),
+		ui.output_text("error_message", inline=True)
+	),
+	ui.card(
+		ui.output_text("error_message_main"),
+		ui.output_table("data_preview")
+	),	
+	width = 350
+	) 
 )
 
 def data_loading_server(input, output, session):
 	df_store = reactive.value(None)
+	error_store = reactive.value("")
 	
 	@output
 	@render.text
@@ -32,6 +38,16 @@ def data_loading_server(input, output, session):
 		if file_info: 
 			return f"Selected file: {file_info[0]["name"]}"
 		return "No file selected"
+	
+	@output
+	@render.text
+	def error_message():
+		return error_store.get()
+	
+	@output
+	@render.text
+	def error_message_main():
+		return error_store.get()
 
 	@reactive.Effect
 	@reactive.event(input.process)
@@ -40,17 +56,40 @@ def data_loading_server(input, output, session):
 		if file_info and len(file_info) > 0:
 			try: 
 				file_ext = file_info[0]["name"].split(".")[-1].lower()
+				file_path = file_info[0]["datapath"]		
+	
 				if file_ext == "csv":
-					df = pd.read_csv(file_info[0]["datapath"])  
+					df = pd.read_csv(file_path)
+  
 				elif file_ext == "json":
-					df = pd.read_json(file_info["datapath"])
+					with open(file_path, "r", encoding = "utf-8") as f:
+						json_data = json.load(f)
+
+					if isinstance(json_data, dict):
+						normalized_data = []
+						for key, value in json_data.items():
+							if isinstance(value, dict):
+								flattened_dict = {f"{key}.{subkey}":subvalue for subkey, subvalue in value.items()}
+								normalized_data.append(flattened_dict)
+							else:
+								normalized_data.append({key:value})
+						df = pd.DataFrame(normalized_data)
+
+					elif isinstance(json_data, list):
+						df = pd.DataFrame(json_data)
+					else: 
+						raise ValueError("Unsupported JSON format")
+
 				else:
-					print("Unsupported file type")
+					error_store.set("Unsupported file type")
+					df_store.set(None)
 					return
 				df_store.set(df)
+				error_store.set("")
+
 			except Exception as e:
-				print(f"Error loading file:{e}")  
-	
+				error_store.set(f"Error loading file: {str(e)}")
+				df_store.set(None)	
 	@output
 	@render.table
 	def data_preview():
