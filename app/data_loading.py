532cce720234f680a8ab8f9aa46f24569d270e0b
nadiawoo <shanghim@gmail.com>
2025-03-10 03:27:23 -0400

    added progress bar

--- app/data_loading.py
+++ app/data_loading.py
@@ -1,4 +1,5 @@
 from shiny import ui, reactive, render
+import time
 import pandas as pd
 import json
 
@@ -35,12 +36,13 @@ data_loading_ui = ui.nav_panel(
 			placeholder = "No file selected",
 		),
 		ui.input_action_button("process", "Process Data"),
+		ui.output_ui("progress"),
 		ui.output_text("file_name"),
 		ui.output_text("error_message", inline=True)
 	),
 	ui.card(ui.output_text("error_message_main"),
 	ui.card(ui.panel_title("Dataframe preview"), ui.output_table("data_preview")),
-	ui.card(ui.panel_title("Summary Statistics"), ui.output_table("summary_stats")),
+	ui.card(ui.panel_title("Summary Statistics"),ui.p("this is for preliminary consideration only"), ui.output_table("summary_stats")),
 	ui.card(ui.panel_title("Data Types of Each Column"),ui.output_table("data_types")),	
 	width = 350
 	) 
@@ -77,35 +79,42 @@ def data_loading_server(input, output, session):
 			try: 
 				file_ext = file_info[0]["name"].split(".")[-1].lower()
 				file_path = file_info[0]["datapath"]		
+				
+				with ui.Progress(min=0, max=100) as p:
+					p.set(10, "Processing file...")
+					start_time = time.time()
 	
-				if file_ext == "csv":
-					df = pd.read_csv(file_path)
-  
-				elif file_ext == "json":
-					with open(file_path, "r", encoding = "utf-8") as f:
-						json_data = json.load(f)
+					if file_ext == "csv":
+						df = pd.read_csv(file_path)
+	  
+					elif file_ext == "json":
+						with open(file_path, "r", encoding = "utf-8") as f:
+							json_data = json.load(f)
 
-					if isinstance(json_data, dict):
-						normalized_data = []
-						for key, value in json_data.items():
-							if isinstance(value, dict):
-								flattened_dict = {f"{key}.{subkey}":subvalue for subkey, subvalue in value.items()}
-								normalized_data.append(flattened_dict)
-							else:
-								normalized_data.append({key:value})
-						df = pd.DataFrame(normalized_data)
+						if isinstance(json_data, dict):
+							normalized_data = []
+							for key, value in json_data.items():
+								if isinstance(value, dict):
+									flattened_dict = {f"{key}.{subkey}":subvalue for subkey, subvalue in value.items()}
+									normalized_data.append(flattened_dict)
+								else:
+									normalized_data.append({key:value})
+							df = pd.DataFrame(normalized_data)
 
-					elif isinstance(json_data, list):
-						df = pd.DataFrame(json_data)
-					else: 
-						raise ValueError("Unsupported JSON format")
+						elif isinstance(json_data, list):
+							df = pd.DataFrame(json_data)
+						else: 
+							raise ValueError("Unsupported JSON format")
 
-				else:
-					error_store.set("Unsupported file type")
-					df_store.set(None)
-					return
-				df_store.set(df)
-				error_store.set("")
+					else:
+						error_store.set("Unsupported file type")
+						df_store.set(None)
+						return
+
+					df_store.set(df)
+					error_store.set("")
+					
+					p.set(100, "Reading Complete!")
 
 			except Exception as e:
 				error_store.set(f"Error loading file: {str(e)}")
